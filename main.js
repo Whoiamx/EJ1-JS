88eae0a3ac4a8fca66220d443873f7ba1f44f628
Whoiamx <gaastontimchuk@gmail.com>
2023-11-17 10:41:29 -0300

    Se finaliza el desafio

--- main.js
+++ main.js
@@ -1,70 +1,93 @@
-// // Deberán realizar los siguientes ejercicios:
+// Deberán realizar los siguientes ejercicios:
 
-// // 1- Crear una función que reciba un número por parámetro e indique en consola si el número es par o impar. ✅
+// 1- Crear una función que reciba un número por parámetro e indique en consola si el número es par o impar. ✅
 
-// function numerType(num) {
-//   if (num % 0) {
-//     return `El ${num} es par`;
-//   } else {
-//     return `El ${num} es impar`;
-//   }
-// }
+function numerType(num) {
+  if (num % 0) {
+    return `El ${num} es par`;
+  } else {
+    return `El ${num} es impar`;
+  }
+}
 
-// console.log(numerType(3));
+console.log(numerType(3));
 
-// // 2- Crear una función que reciba dos números por parámetro e indique en consola que número es mayor, y si ninguno lo es, indicar por consola que son iguales.✅
+// 2- Crear una función que reciba dos números por parámetro e indique en consola que número es mayor, y si ninguno lo es, indicar por consola que son iguales.✅
 
-// function maxOrMin(num1, num2) {
-//   if (num1 === num2) {
-//     return ` El numero ${num1} y El numero ${num2} son iguales`;
-//   } else if (num1 > num2) {
-//     return `El numero ${num1} es mayor que el numero ${num2}`;
-//   } else {
-//     return `El numero ${num2} es mayor que el numero ${num1}`;
-//   }
-// }
+function maxOrMin(num1, num2) {
+  if (num1 === num2) {
+    return ` El numero ${num1} y El numero ${num2} son iguales`;
+  } else if (num1 > num2) {
+    return `El numero ${num1} es mayor que el numero ${num2}`;
+  } else {
+    return `El numero ${num2} es mayor que el numero ${num1}`;
+  }
+}
 
-// console.log(maxOrMin(100, 200));
+console.log(maxOrMin(100, 200));
 
-// // 3- Crear una función que reciba un número por parámetro e indique en consola si ese número es múltiplo de 5. ✅
+// 3- Crear una función que reciba un número por parámetro e indique en consola si ese número es múltiplo de 5. ✅
 
-// function multipleFive(number) {
-//   if (number % 5 === 0) {
-//     return `El ${number} ES multiplo de 5`;
-//   } else {
-//     return `El ${number} NO es multiplo de 5`;
-//   }
-// }
+function multipleFive(number) {
+  if (number % 5 === 0) {
+    return `El ${number} ES multiplo de 5`;
+  } else {
+    return `El ${number} NO es multiplo de 5`;
+  }
+}
 
-// console.log(multipleFive(122));
+console.log(multipleFive(122));
 
 // 4- Crear una función que reciba un número por parámetro e imprima por consola todos los números desde el 0 hasta llegar a ese número.✅
 
-// function counter(init) {
-//   for (i = 0; i <= init; i++) {
-//     console.log(`Se imprime el numero hasta el ${i}`);
-//   }
-// }
-// counter(43);
+function counter(init) {
+  for (i = 0; i <= init; i++) {
+    console.log(`Se imprime el numero hasta el ${i}`);
+  }
+}
+counter(43);
 
 // 5 - Crear una función que reciba una palabra y un número por parámetro e imprima por consola  esa palabra la cantidad correspondiente al número indicado. ✅
-// function wordsLenght(word, numero) {
-//   for (i = 0; i < numero; i++) {
-//     console.log(`Se imprime la palabra ${word} la cantidad de  ${numero}`);
-//   }
-// }
+function wordsLenght(word, numero) {
+  for (i = 0; i < numero; i++) {
+    console.log(`Se imprime la palabra ${word} la cantidad de  ${numero}`);
+  }
+}
 
-// wordsLenght("Javascript", 100);
+wordsLenght("Javascript", 100);
 
 // 6 - Crear una función que reciba un array por parámetro e imprima por consola todos los valores de ese array. ✅
 
-// function getArray(myArray) {
-//   let array = [myArray];
-//   console.log(myArray);
-// }
+function getArray(myArray) {
+  let array = [myArray];
+  console.log(myArray);
+}
 
-// getArray([1, 2, 4, "Hola", true]);
+getArray([1, 2, 4, "Hola", true]);
 
 // 7 - Crear una función que reciba un array con 10 números e imprima por consola todos los valores de ese array, menos el que se encuentre en la 5ta posición del mismo. Ayuda: Recuerden que el primer índice de un array es "0".
 
-// 8 - Crea una función que reciba un array de números y un número por parámetro e imprima por consola cada número del array multiplicado por el número pasado por parámetro.
+const cadenaTen = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function arrayFive(arrayn) {
+  for (let i = 0; i < arrayn.length; i++) {
+    if (i === 5) {
+      continue;
+    }
+    console.log(arrayn[i]);
+  }
+}
+
+arrayFive(cadenaTen);
+
+// 8 - Crea una función que reciba un array de números y un número por parámetro e imprima por consola cada número del array multiplicado por el número pasado por parámetro. ✅
+
+const arrayNum = [1, 2, 3, 5, 6, 7];
+
+const multiArr = (array, num) => {
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i] * num);
+  }
+};
+
+multiArr(arrayNum, 5);
